bb40de04ad2a0247eb2e654300efb5d229888934
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2022-09-15 14:25:35 +0700

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Listing;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+        Listing::factory(6)->create();
+
+        //Listing::create([
+            //'title' => 'Laravel Senior Developer',
+            //'tags' => 'laravel, javascript',
+            //'company' => 'Acme Corp',
+            //'location' => 'Boston, MA',
+            //'email' => 'email1@email.com',
+            //'website' => 'https://www.acme.com',
+            //'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non possimus sequi magni illo repellendus odio deserunt fugit soluta. Odit est laborum dolorum consectetur fuga iste numquam aperiam. Ex, obcaecati eum!'
+        //]);
+
+        //Listing::create([
+            //'title' => 'Full-Stack Engineer',
+            //'tags' => 'laravel, backend, api',
+            //'company' => 'Stark Industries',
+            //'location' => 'New York, NY',
+            //'email' => 'email2@email.com',
+            //'website' => 'https://www.starkindustries.com',
+            //'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non possimus sequi magni illo repellendus odio deserunt fugit soluta. Odit est laborum dolorum consectetur fuga iste numquam aperiam. Ex, obcaecati eum!'
+        //]);
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models;
+use App\Http\Controllers\ListingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
