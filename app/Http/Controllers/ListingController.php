f1c81b4b75b0f04ad96878397128628f1304342c
Ibrahim Syah <ibrahim.syah.q@gmail.com>
2022-09-20 17:06:13 +0700

    saved old data when invalid form

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Models\Listing;
 
 class ListingController extends Controller
@@ -20,4 +21,27 @@ class ListingController extends Controller
             'listing' => $listing
         ]);
     }
+
+    // Show Create Form
+    public function create() {
+        return view('listings.create');
+    }
+
+    // Store Listing Data
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Listing created succesfully!');
+    }
 }
+
